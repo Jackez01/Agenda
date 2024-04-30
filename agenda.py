1c3ad385bfba42886d3dace5cb898f7fee02b6e4
Jackez01 <lucasgefreitas@gmail.com>
2024-04-30 15:39:12 -0300

    Estrutura

---
+++ agenda.py
@@ -0,0 +1,25 @@
+def adiconar_contato(contato, telefone, email):
+
+agenda = []
+while True:
+  print("\n Agenda")
+  print("1. Adicionar novo contato")
+  print("2. Contatos cadastrados")
+  print("3. Editar contato")
+  print("4. Marcar um contato como favorito")
+  print("5. Favoritos")
+  print("6. Apagar contato")
+
+  escolha = input("Digite sua escolha: ")
+
+  if escolha == "1":
+
+  elif escolha == "2":
+
+  elif escolha == "3":
+
+  elif escolha == "4":
+
+  elif escolha == "5":
+
+  elif escolha == "6":
